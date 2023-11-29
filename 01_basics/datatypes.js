9b1c929ebf2472fc8736f304d2d7dd8f31af8064
Abhijeetratan <134995703+Abhijeetratan@users.noreply.github.com>
2023-11-29 23:30:36 +0000

    datatypes

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -6,3 +6,16 @@ console.log(score)
 
 let valueInNumber = Number(score)
 console.log(typeof valueInNumber);
+//"33"=>33
+//"33abc"=>NAN
+//true=>1;false=>0()
+let IsLoggedIn = "abhi"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+//1=> true;0 =>false
+//""=> false
+//"hitesh"=> true
+let someNumber = 44
+let stringNumber = String(someNumber)
+console, log(stringNumber)
+console.log(typeof stringNumber)
