b8060f7b1eae329da3c192154c008fe5581ffa9a
Abhijeetratan <134995703+Abhijeetratan@users.noreply.github.com>
2023-11-29 22:59:04 +0000

    commit

---
+++ 01_basics/variable.js
@@ -0,0 +1,11 @@
+const accountId=14453
+let accountEmail="abhi@gmail.com"
+var accountPassword="123"
+accountCity="bihar"
+let accountState;
+
+accountEmail="abc@gmail.com"
+accountPassword="222221"
+accountCity="U.P"
+
+console.table([accountId,accountPassword,accountEmail,accountCity,accountState])
